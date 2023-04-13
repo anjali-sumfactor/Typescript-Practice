8fa127a03d76d3ff9daa58c34bb8bb3de5b96f8b
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-13 16:42:25 +0530

    practice

--- app.js
+++ app.js
@@ -18,3 +18,32 @@ var n = 20;
 console.log(n);
 var bool = true;
 console.log(bool);
+//Array in typescript:-
+var data = ["Jonny", "Mario", "John"];
+data.push("Peter");
+// data.push(100);
+// data.push(true);
+console.log(data);
+var users = {
+    name: "bruce",
+    age: 24,
+    address: "USA",
+};
+// users.name = 200;
+users.name = "peter";
+// let users: any = {
+//     name: "bruce",
+//     age: 24,
+//     address: "USA",
+// }
+console.log(users);
+//Union type:-
+var datas = "ajay";
+datas = 40;
+// datas = true;
+console.log(datas);
+var userObj = {
+    name: "ankita",
+    age: 23,
+};
+console.log(userObj);
--- app.ts
+++ app.ts
@@ -15,5 +15,53 @@ console.log(num);
 var n: number = 20;
 console.log(n);
 
-var bool:boolean = true;
+var bool: boolean = true;
 console.log(bool);
+
+//Array in typescript:-
+let data: string[] = ["Jonny", "Mario", "John"];
+data.push("Peter");
+// data.push(100);
+// data.push(true);
+console.log(data);
+
+//Object in typescript:-
+interface usersTyped {
+    name: string,
+    age: number,
+    address: any,
+}
+
+let users: usersTyped = {
+    name: "bruce",
+    age: 24,
+    address: "USA",
+}
+// users.name = 200;
+users.name = "peter";
+
+// let users: any = {
+//     name: "bruce",
+//     age: 24,
+//     address: "USA",
+// }
+
+console.log(users);
+
+//Union type:-
+let datas: string | number = "ajay";
+datas = 40;
+// datas = true;
+console.log(datas);
+
+//Interface in typescript:-
+interface userObjType {
+    name: string,
+    age: number,
+}
+
+let userObj: userObjType = {
+    name: "ankita",
+    age: 23,
+}
+console.log(userObj);
