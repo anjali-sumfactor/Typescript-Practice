8669f9d8a4f889d5df56a54f2883f56db7db9ad2
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-17 16:48:23 +0530

    practice

--- Tuples/app.js
+++ Tuples/app.js
@@ -1,3 +1,8 @@
 var ourTuples;
 ourTuples = [23, true, "tuples in typescript"];
+// ourTuples = [true, 45, "tuples in ts"]; //through an error
 console.log(ourTuples);
+var ourTuple;
+ourTuple = [5, false, 'Coding God was here'];
+ourTuple.push('Something new and wrong');
+console.log(ourTuple);
--- Tuples/app.ts
+++ Tuples/app.ts
@@ -1,3 +1,9 @@
 let ourTuples: [number, boolean, string];
 ourTuples = [23, true, "tuples in typescript"];
+// ourTuples = [true, 45, "tuples in ts"]; //through an error
 console.log(ourTuples);
+
+let ourTuple: [number, boolean, string];
+ourTuple = [5, false, 'Coding God was here'];
+ourTuple.push('Something new and wrong');
+console.log(ourTuple);
