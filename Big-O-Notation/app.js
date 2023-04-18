0c1899921ab602773d45e8cfff277fe27fef2372
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-18 16:36:59 +0530

    practice

--- Big-O-Notation/app.js
+++ Big-O-Notation/app.js
@@ -1,10 +1,10 @@
-// function linearFunc(arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//         console.log(1000 * 100000);
-//     }
-// }
-// const arr = [1, 2, 3, 4, 5, 6];
-// linearFunc(arr);
+function linearFunc(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(1000 * 100000);
+    }
+}
+const arr = [1, 2, 3, 4, 5, 6];
+linearFunc(arr);
 
 //binary search:-
 let array = [];
