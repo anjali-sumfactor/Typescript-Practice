2ecefcf735bae9233949ecd75e3670ed01ff9455
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-20 13:35:16 +0530

    practice

---
+++ Guards/app.js
@@ -0,0 +1,62 @@
+//your code needs ts guards whenever you want to allow only a specific type or a block of code and it can take many different forms such as instant self operators, typeof operator and even user defined type guards.
+function add(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+}
+console.log(add(5, 5));
+//2.instantof guard:-
+var Banana = /** @class */ (function () {
+    function Banana() {
+    }
+    Banana.prototype.isTasty = function () {
+        return true;
+    };
+    return Banana;
+}());
+var Grapes = /** @class */ (function () {
+    function Grapes() {
+    }
+    Grapes.prototype.isJuicy = function () {
+        return true;
+    };
+    return Grapes;
+}());
+function buyFruits(fruit) {
+    var price = 0;
+    if (fruit instanceof Banana) {
+        price = fruit.isTasty() ? 5 : 10;
+    }
+    return price;
+}
+var banana = new Banana();
+console.log(buyFruits(banana));
+//3. in guard:-
+var Apple = /** @class */ (function () {
+    function Apple() {
+    }
+    Apple.prototype.isTasty = function () {
+        return true;
+    };
+    return Apple;
+}());
+var Watermelon = /** @class */ (function () {
+    function Watermelon() {
+    }
+    Watermelon.prototype.isJuicy = function () {
+        return true;
+    };
+    return Watermelon;
+}());
+function buyFruit(fruits) {
+    var price = 0;
+    if ("isTasty" in fruits) {
+        price = fruits.isTasty() ? 50 : 30;
+    }
+    if ("isJuicy" in fruits) {
+        price = fruits.isJuicy() ? 70 : 90;
+    }
+    return price;
+}
+var watermelon = new Watermelon();
+console.log(buyFruit(watermelon));
---
+++ Guards/app.ts
@@ -0,0 +1,65 @@
+//your code needs ts guards whenever you want to allow only a specific type or a block of code and it can take many different forms such as instant self operators, typeof operator and even user defined type guards.
+
+//1.typeof guard:-
+type alphanumeric = string | number;
+
+function add(a: alphanumeric, b: alphanumeric) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+} console.log(add(5, 5));
+
+//2.instantof guard:-
+class Banana {
+    isTasty(): Boolean {
+        return true;
+    }
+}
+
+class Grapes {
+    isJuicy(): Boolean {
+        return true;
+    }
+}
+
+type Fruit = Banana | Grapes;
+
+function buyFruits(fruit: Fruit): number {
+    let price = 0;
+    if (fruit instanceof Banana) {
+        price = fruit.isTasty() ? 5 : 10;
+    }
+    return price;
+}
+const banana = new Banana();
+console.log(buyFruits(banana));
+
+//3. in guard:-
+class Apple {
+    isTasty(): Boolean {
+        return true;
+    }
+}
+
+class Watermelon {
+    isJuicy(): Boolean {
+        return true;
+    }
+}
+
+type Fruits = Apple | Watermelon;
+
+function buyFruit(fruits: Fruits): number {
+    let price = 0;
+
+    if ("isTasty" in fruits) {
+        price = fruits.isTasty() ? 50 : 30;
+    }
+
+    if ("isJuicy" in fruits) {
+        price = fruits.isJuicy() ? 70 : 90;
+    }
+    return price;
+}
+const watermelon = new Watermelon();
+console.log(buyFruit(watermelon));
